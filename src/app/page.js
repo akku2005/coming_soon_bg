a65aeb794eb0edcb424fe347c2d730d742fdf9d1
akku2005 <asakashsahu20@gmail.com>
2024-03-03 01:22:54 +0530

    video add

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
             playsInline 
             speed="0.5"
           >
-            <source src="homepage/bgg.mp4" type="video/mp4" />
+            <source src="homepage/bg.mp4" type="video/mp4" />
           </video>
           <div className="absolute inset-0">
             <Navbar />
